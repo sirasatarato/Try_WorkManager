4775c13af7322ec17a0636db2c7c5e12a8be913c
sirasatarato <sirasatarato@gmail.com>
2020-09-21 09:21:13 +0900

    Compose Main xml

--- try_workmanager/app/src/main/java/com/silso/try_workmanager/MainActivity.kt
+++ try_workmanager/app/src/main/java/com/silso/try_workmanager/MainActivity.kt
@@ -2,6 +2,7 @@ package com.silso.try_workmanager
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.work.WorkManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
