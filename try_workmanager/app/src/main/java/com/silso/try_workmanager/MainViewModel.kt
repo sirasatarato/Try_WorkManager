01cff4f57228cdd33029bf92836d9efb4cb048b6
sirasatarato <sirasatarato@gmail.com>
2020-09-21 09:26:30 +0900

    Create MainViewModel

---
+++ try_workmanager/app/src/main/java/com/silso/try_workmanager/MainViewModel.kt
@@ -0,0 +1,10 @@
+package com.silso.try_workmanager
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+
+class MainViewModel(application: Application): AndroidViewModel(application) {
+    var workManager: WorkManager = WorkManager.getInstance(application.applicationContext)
+}
